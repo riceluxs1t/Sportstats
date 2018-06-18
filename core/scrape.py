caabc1d1af1d7e5ae8eb9a0ee07cdb7503d430a2
riceluxs1t <riceluxs1t@gmail.com>
2018-06-19 02:48:07 +0900

    added comments.

--- core/scrape.py
+++ core/scrape.py
@@ -2,7 +2,13 @@ from bs4 import BeautifulSoup
 
 
 class ManualScraper(object):
+    """
+    Given a path to the HTML page of eloratings.net/XXXX_result,
+    parses and returns a list of match data which is a list of
+    the following fields in the listed order.
 
+    TODO: return a Pandas frame.
+    """
     FIELDS = [
         "HomeTeam",
         "AwayTeam",
@@ -25,21 +31,38 @@ class ManualScraper(object):
     def __init__(self):
         self.num_fields = len(self.FIELDS)
 
-    def get_raw_matches(self, year_result_file_path):
-        html_page = self.get_html_page(year_result_file_path)
+    def get_raw_matches(self, file_path):
+        """
+        The function that provides the main service.
+        :param file_path: the path to an eloratings.net results page.
+        :return: a list of match data.
+        """
+        html_page = self.get_html_page(file_path)
         html_tree = self.get_html_tree(html_page)
         all_match_divs = self.get_all_match_divs(html_tree)
         matches = self.process_match_divs(all_match_divs)
         return matches
 
     def get_html_page(self, file_path):
+        """
+        Reads in a file at the specified path
+        and returns its string representation.
+        """
         with open(file_path, 'r') as file:
             return file.read()
 
     def get_html_tree(self, page):
+        """
+        Given the string representation of an HTML file,
+        returns its DOM tree representation using BeaultifulSoup.
+        """
         return BeautifulSoup(page, "html.parser")
 
     def get_all_match_divs(self, html_tree):
+        """
+        Given an HTML DOM tree, extracts and returns all the div tags
+        that correspond to individual match data.
+        """
         even_indexed_matches = html_tree.findAll(
             "div", {"class": "ui-widget-content slick-row even"}
         )
@@ -51,6 +74,10 @@ class ManualScraper(object):
         return even_indexed_matches + odd_indexed_matches
 
     def process_match_divs(self, matches):
+        """
+        Processes each Match div and returns the list of
+        processed results.
+        """
         result = []
         for match in matches:
             result.append(
@@ -59,12 +86,20 @@ class ManualScraper(object):
         return result
 
     def process_single_match_div(self, match):
+        """
+        Processes each Match div.
+        """
         info_divs = match.findAll("div")
 
         def extract_info(index, extractor_function):
+            """
+            A higher-order function that processes each column div.
+            (each row = individual match, each column = some data about the match)
+            """
             tag_contents = info_divs[index].contents
             return extractor_function(tag_contents)
 
+        # simple extractor functions.
         a_tag_extractor = lambda contents, index: contents[index].text
         a_tag_extractor_first = lambda contents: a_tag_extractor(contents, 0)
         a_tag_extractor_second = lambda contents: a_tag_extractor(contents, 2)
@@ -73,6 +108,7 @@ class ManualScraper(object):
         string_tag_extractor_second = lambda contents: string_tag_extractor(contents, 2)
 
         def date_extractor(contents):
+            """An extractor function for the date div."""
             # e.g. ['January 4', <br/>, '2017']
             month_day = contents[0]
             year = contents[2]
@@ -80,6 +116,7 @@ class ManualScraper(object):
             return '-'.join([year] + month_day.split(' '))
 
         def custom_int(negative_integer):
+            """A custom function that processes numerical data."""
             if len(negative_integer) == 1:
                 return 0
 
