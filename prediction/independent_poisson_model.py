d77ecad2654f8a975584dfb6b6082096af268215
frank217 <dw18@rice.edu>
2018-06-29 23:54:38 -0500

    tournament bottle neck fixed

--- prediction/independent_poisson_model.py
+++ prediction/independent_poisson_model.py
@@ -86,7 +86,7 @@ class IndependentPoissonModel(object):
 
         l_A = (uA_B + vB_A) / 2.0
         l_B = (uB_A + vA_B) / 2.0
-        return l_A,l_B
+        return [l_A,l_B]
 
 
     def sample(self, num_iters):
@@ -101,11 +101,17 @@ class IndependentPoissonModel(object):
             "croatia","denmark",
             "sweden","switzerland",
             "colombia","england"]
+        E_dict = defaultdict(int)
+        for i in range(len(given_round16)):
+            for j in range(i+1,len(given_round16)):
+                team1 = min(given_round16[i],given_round16[j])
+                team2 = max(given_round16[i],given_round16[j])
+                E_dict[(team1,team2)] = self.getExpectation(team1,team2)                
 
         winner_dict = defaultdict(int)
         
         for iteration in range(num_iters):
-            print("iteration :",iteration)
+            # print("iteration:",iteration)
             predict_final = []
             predict_qf = []
             predict_round8 = []
@@ -113,26 +119,28 @@ class IndependentPoissonModel(object):
                 #Guess winner for each game in round 16
                 home_team = given_round16[game_num*2]
                 away_team = given_round16[game_num*2+1]
-                predict_round8.append(self.getWinner(home_team,away_team))
+                predict_round8.append(self.getWinner(home_team,away_team,E_dict))
             for game_num in range(4):
                 home_team = predict_round8[game_num*2]
                 away_team = predict_round8[game_num*2+1]
-                predict_qf.append(self.getWinner(home_team,away_team))
+                predict_qf.append(self.getWinner(home_team,away_team,E_dict))
             for game_num in range(2):
                 home_team = predict_qf[game_num*2]
                 away_team = predict_qf[game_num*2+1]
-                predict_final.append(self.getWinner(home_team,away_team))
+                predict_final.append(self.getWinner(home_team,away_team,E_dict))
             home_team,away_team = predict_final
-            winner_dict[self.getWinner(home_team,away_team)] += 1
+            winner_dict[self.getWinner(home_team,away_team,E_dict)] += 1
 
         
         for key in winner_dict:
             winner_dict[key] /= num_iters                
         return winner_dict
 
-    def getWinner(self,home_team,away_team):
+    def getWinner(self,home_team,away_team,E_dict):
         """Use Expectation of Poisson to get a winner of the game"""
-        l_A,l_B = self.getExpectation(home_team,away_team)
+        team1 = min(home_team,away_team)
+        team2 = max(home_team,away_team)
+        l_A,l_B = E_dict[(team1,team2)]
         home_team_score = numpy_poisson_distribution(l_A,1)
         away_team_score = numpy_poisson_distribution(l_B,1)
         if home_team_score > away_team_score:
@@ -257,5 +265,39 @@ iteration 100:
     'france': 0.04,
     'england': 0.01})
 iteration 1000:
+defaultdict(int,
+            {'argentina': 0.112,
+             'portugal': 0.074,
+             'brazil': 0.217,
+             'switzerland': 0.049,
+             'croatia': 0.037,
+             'russia': 0.013,
+             'spain': 0.183,
+             'belgium': 0.066,
+             'colombia': 0.091,
+             'uruguay': 0.025,
+             'denmark': 0.025,
+             'france': 0.036,
+             'mexico': 0.026,
+             'sweden': 0.027,
+             'england': 0.018,
+             'japan': 0.001})
+iteration 10000
+            {'switzerland': 0.0693,
+             'brazil': 0.076,
+             'uruguay': 0.1076,
+             'croatia': 0.0557,
+             'russia': 0.0966,
+             'colombia': 0.0439,
+             'sweden': 0.0573,
+             'argentina': 0.0559,
+             'france': 0.0815,
+             'belgium': 0.1315,
+             'spain': 0.0201,
+             'mexico': 0.0313,
+             'denmark': 0.0527,
+             'portugal': 0.0545,
+             'england': 0.0496,
+             'japan': 0.0165})
 
 """
